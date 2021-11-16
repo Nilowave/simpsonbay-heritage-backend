a156b662a82102e29eec44582d41e09c37e10fc6
Danilo <danilo@mediamonks.com>
2021-11-15 22:38:38 -0800

    complete intro page; started comments component

---
+++ plugins/pdf-convert/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin pdf-convert
+
+A quick description of pdf-convert.
---
+++ plugins/pdf-convert/controllers/pdf-convert.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * pdf-convert.js controller
+ *
+ * @description: A set of functions called "actions" of the `pdf-convert` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
